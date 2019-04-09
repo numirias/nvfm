f35802524c20b443fc02debb8960fa2bd701e7fd
numirias <numirias@users.noreply.github.com>
2019-04-09 17:00:28 +0200

    Add more basic tests

--- test/conftest.py
+++ test/conftest.py
@@ -1,3 +1,4 @@
+from contextlib import contextmanager
 import os
 from pathlib import Path
 import subprocess
@@ -14,30 +15,21 @@ def plugin_dir(tmpdir_factory):
     return tmpdir_factory.mktemp('test_plugin')
 
 
-@pytest.fixture(scope='module')
-def home_dir(plugin_dir):
-    home = Path(str(plugin_dir)) / 'alice'
-    Path(home / '.local/share').mkdir(parents=True)
-    return home
-
-
 @pytest.fixture(scope='module', autouse=True)
-def set_environment(plugin_dir, home_dir):
+def set_environment(plugin_dir):
     os.environ['NVIM_RPLUGIN_MANIFEST'] = str(plugin_dir.join('rplugin.vim'))
     os.environ['NVIM_PYTHON_LOG_FILE'] = '/tmp/log'
     os.environ['NVIM_PYTHON_LOG_LEVEL'] = 'DEBUG'
     os.environ['NVFM_LOG_FILE'] = '/tmp/nvfm.log'
     os.environ['NVFM_LOG_LEVEL'] = 'DEBUG'
-    os.environ['HOME'] = str(home_dir)
     yield
 
 
-@pytest.fixture(scope='function')
-def vim(plugin_dir):
+def start_vim(socket_base):
     import random
-    socket_path = str(plugin_dir.join('nvim_socket' + hex(random.randint(0x10000000,0xffffffff))[2:]))
+    socket_path = str(socket_base.join('nvim_socket' + hex(random.randint(0x10000000,0xffffffff))[2:]))
     argv = ['nvfm', '--listen', socket_path, '--headless']
-    p = subprocess.Popen(argv)
+    subprocess.Popen(argv)
     print('waiting for socket creation...')
     for i in range(50):
         if Path(socket_path).is_socket():
@@ -49,5 +41,23 @@ def vim(plugin_dir):
     print('attached to socket at:', socket_path)
     print('rtp:', vim.eval('&rtp'))
     print('py3 plugins:', vim.call('remote#host#PluginsForHost', 'python3'))
+    return vim
+
+
+@pytest.fixture(scope='function')
+def vim_ctx(plugin_dir):
+    @contextmanager
+    def func():
+        vim = start_vim(plugin_dir)
+        try:
+            yield vim
+        finally:
+            vim.quit()
+    return func
+
+
+@pytest.fixture(scope='function')
+def vim(plugin_dir):
+    vim = start_vim(plugin_dir)
     yield vim
     vim.quit()
--- test/test_plugin.py
+++ test/test_plugin.py
@@ -1,5 +1,22 @@
 import os
+from pathlib import Path
 import re
+
+import pytest
+
+
+@pytest.fixture
+def tree(tmpdir_factory):
+    tree = Path(str(tmpdir_factory.mktemp('tree')))
+    base = (tree / 'base')
+    base.mkdir()
+    (base / 'aa').mkdir()
+    (base / 'aa' / 'aa_aa').mkdir()
+    (base / 'aa' / 'aa_aa' / 'aa_aa_f').touch()
+    (base / 'bb').write_text('bb_line_1\nbb_line_2')
+    (base / 'cc').mkdir()
+    (base / 'dd').touch()
+    return base
 
 
 def test_startup(vim):
@@ -11,3 +28,48 @@ def test_tabline(vim):
     # Strip markup
     tabline = re.sub('%#.*?#', '', tabline)
     assert os.getcwd() in tabline
+
+
+def test_panels(tree, vim_ctx):
+    os.environ['NVFM_START_PATH'] = str(tree)
+
+    with vim_ctx() as vim:
+        assert len(vim.windows) == 3
+        left, mid, right = vim.windows
+
+        lines = left.buffer[:]
+        assert len(lines) == 1
+        assert 'base' in lines[0]
+
+        lines = mid.buffer[:]
+        assert len(lines) == 4
+        assert 'aa' in lines[0]
+        assert 'bb' in lines[1]
+        assert 'cc' in lines[2]
+
+        lines = right.buffer[:]
+        assert len(lines) == 1
+        assert 'aa_aa' in lines[0]
+
+
+def test_navigation(tree, vim_ctx):
+    os.environ['NVFM_START_PATH'] = str(tree)
+    with vim_ctx() as vim:
+        left, mid, right = vim.windows
+        vim.feedkeys('j')
+        assert right.buffer[:] == ['bb_line_1', 'bb_line_2']
+        vim.feedkeys('j')
+        assert re.match(r'\(.*empty.*\)', '\n'.join(right.buffer[:]))
+        vim.feedkeys('j')
+        assert re.match(r'\(.*empty.*\)', '\n'.join(right.buffer[:]))
+        # Go to top
+        vim.feedkeys('gg')
+        # Enter dir "aaa"
+        vim.feedkeys('l')
+        assert 'aa' in left.buffer[:][0]
+        assert 'aa_aa' in mid.buffer[:][0]
+        assert 'aa_aa_f' in right.buffer[:][0]
+        vim.feedkeys('h')
+        assert 'base' in left.buffer[:][0]
+        assert 'aa' in mid.buffer[:][0]
+        assert 'aa_aa' in right.buffer[:][0]
