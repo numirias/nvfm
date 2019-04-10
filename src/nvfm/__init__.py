8cbb25451d63d1b42c034e7acf812c9240b9290c
numirias <numirias@users.noreply.github.com>
2019-04-10 06:49:54 +0200

    Fix coverage measurement

--- src/nvfm/__init__.py
+++ src/nvfm/__init__.py
@@ -1,1 +1,13 @@
+import sys
+
 from .plugin import Plugin
+
+
+# Sanity check for combined coverage report
+version = sys.version_info[:2]
+if version == (3, 5):
+    assert True
+if version == (3, 6):
+    assert True
+if version == (3, 7):
+    assert True
